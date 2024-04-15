1274a6ead078391eb12ff5e7919c04eab485c8e4
DopeKS <kyusang.dev@gmail.com>
2024-04-15 10:11:36 +0900

    computed property

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -11,13 +11,34 @@ var greeting = "Hello, playground"
 
 class BankAccount {
     // 프로퍼티
-    var accountBlance: Float = 0
     var accountNumber: Int = 0
+    var accountBalance: Float = 0
+    let fees: Float = 25.00
+    
+    var balanceLessFees: Float {
+        get {
+            return accountBalance - fees
+        }
+    }
+    
+    init() {
+        accountNumber = 0
+        accountBalance = 0
+    }
+    
+    init(number: Int, balance: Float) {
+        accountNumber = number
+        accountBalance = balance
+    }
+    
+    deinit {
+        //필요한 정리작업을 수행
+    }
     
     // 인스턴스 메서드
     func displayBalance() -> Void {
         print("Number \(accountNumber)")
-        print("Current balance is \(accountBlance)")
+        print("Current balance is \(accountBalance)")
     }
     
     // 타입 메서드, 전역함수 || 전역 메소트
@@ -30,7 +51,7 @@ class BankAccount {
 
 var bankAccount = BankAccount()
 
-print(bankAccount.accountBlance)
+print(bankAccount.accountBalance)
 print(bankAccount.accountNumber)
 bankAccount.displayBalance()
 
