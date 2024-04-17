fe1531f8997dda33203b0e9795390a072af6023d
DopeKS <kyusang.dev@gmail.com>
2024-04-17 13:09:32 +0900

    Initialize DemoProject

---
+++ DemoProject/DemoProject/DemoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoProjectApp.swift
+//  DemoProject
+//
+//  Created by 황규상 on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
