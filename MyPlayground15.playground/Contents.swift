8396c9403732b05efe5db0c79f3c5b2f5a422f80
DopeKS <kyusang.dev@gmail.com>
2024-04-17 10:07:51 +0900

    LocalizedError

--- MyPlayground15.playground/Contents.swift
+++ MyPlayground15.playground/Contents.swift
@@ -1,11 +1,29 @@
+import Foundation
 let connectionOK = true
 let connectionSpeed = 30.00
-let fileFound = false
+let fileFound = true
 
-enum FileTransferError: Error {
+// LOcalizable.string(Korean)
+// "noConnection" = "연결이 없습니다."
+// "lowBandwidth" = "속도가 느립니다."
+// "fileNotFound" = "파일을 찾을 수 없습니다.."
+
+// LocalizedError: 소비자한테 에러를 친절하게 제공하고 싶을 때 사용
+enum FileTransferError: Error, LocalizedError {
     case noConnection
     case lowBandwidth
     case fileNotFound
+    
+    var errorDescription: String? {
+        switch self {
+        case .noConnection:
+            return NSLocalizedString("noConnection", comment: "no Connection")
+        case .lowBandwidth:
+            return NSLocalizedString("lowBandwidth", comment: "low Bandwidth")
+        case .fileNotFound:
+            return NSLocalizedString("fileNotFound", comment: "file Not Found")
+        }
+    }
 }
 
 func fileTransfer() throws {
@@ -24,12 +42,16 @@ func fileTransfer() throws {
 }
 
 func sendFile() -> String {
+    
+    defer {
+        // 파일 닫기 등 종료 준비
+        print("end of sendFile")
+    }
+    
     do {
         try fileTransfer()
     } catch FileTransferError.noConnection {
-        return "No Connection"
-    } catch FileTransferError.lowBandwidth {
-        return "Speed too low"
+        return "Connection Problems"
     } catch FileTransferError.fileNotFound {
         return "File Not Found"
     } catch {
@@ -37,3 +59,5 @@ func sendFile() -> String {
     }
     return "Successful transfer"
 }
+
+print(sendFile())
