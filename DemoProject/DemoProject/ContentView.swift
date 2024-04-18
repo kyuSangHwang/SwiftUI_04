411731c4e1aa3f165a712aced069b15e634fa336
DopeKS <kyusang.dev@gmail.com>
2024-04-18 11:16:39 +0900

    Clear

--- DemoProject/DemoProject/ContentView.swift
+++ DemoProject/DemoProject/ContentView.swift
@@ -10,15 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack {
-            Image(systemName: "airplane")
-            Text("Flight times:")
-            Text("London")
-        }
-        .font(.largeTitle)
-        .frame(width: 300)
-        .lineLimit(1...4)
-        .transaction {
-            $0.disablesAnimations = true // $0 : 익명 파라미터 $0, $1, $2.. 이런식으로
+            
         }
     }
 }
