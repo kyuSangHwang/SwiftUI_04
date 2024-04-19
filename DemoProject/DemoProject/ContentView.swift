901460e8632888e92f3b7c9f1fba4939a682923f
DopeKS <kyusang.dev@gmail.com>
2024-04-19 13:50:23 +0900

    Create Github repository api async function

--- DemoProject/DemoProject/ContentView.swift
+++ DemoProject/DemoProject/ContentView.swift
@@ -11,20 +11,26 @@ struct Repository: Codable, Identifiable {
     let id: Int
     let name: String
     let description: String?
-    let stargazersCount: Int
 }
 
 struct ContentView: View {
-    @State private var username = "jmbae"
+    @State private var username = "kyuSangHwang"
     @State private var repositories: [Repository] = []
-
+    
     var body: some View {
         VStack {
             TextField("Github username:", text: $username)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding()
             Button("Fetch Data") {
-
+                Task {
+                    do {
+                        async let fetchRepositoris = fetchRepositories(username: username)
+                        repositories = try await fetchRepositoris
+                    } catch {
+                        print("Error: \(error)")
+                    }
+                }
             }
             List(repositories) { repo in
                 VStack(alignment: .leading) {
@@ -32,13 +38,19 @@ struct ContentView: View {
                         .font(.headline)
                     Text(repo.description ?? "No description")
                         .font(.subheadline)
-                    Text("Stars: \(repo.stargazersCount)")
                 }
             }
-
+            
         }
     }
+    
+    func fetchRepositories(username: String) async throws -> [Repository] {
+        let url = URL(string: "https://api.github.com/users/\(username)/repos")!
+        let (data, _) = try await URLSession.shared.data(from: url)
+        return try JSONDecoder().decode([Repository].self, from: data)
+    }
 }
+
 
 #Preview {
     ContentView()
