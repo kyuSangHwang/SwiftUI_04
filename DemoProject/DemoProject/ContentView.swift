46d506602457fd8da5c04ffe5eb311ab19e3d4cd
DopeKS <kyusang.dev@gmail.com>
2024-04-19 11:32:46 +0900

    Clear

--- DemoProject/DemoProject/ContentView.swift
+++ DemoProject/DemoProject/ContentView.swift
@@ -7,27 +7,10 @@
 
 import SwiftUI
 
-class DemoData: ObservableObject {
-    @Published var userCount = 0
-    
-    func increment() {
-        userCount += 1
-    }
-}
-
 struct ContentView: View {
-    @StateObject var demoData: DemoData = DemoData()
-    
     var body: some View {
         VStack {
-            Text("you are user number \(demoData.userCount)")
             
-            Button(action: {
-                demoData.increment()
-            }) {
-                Text("Increment")
-                    .font(.largeTitle)
-            }
         }
     }
 }
