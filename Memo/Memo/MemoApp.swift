d8d41244a225bd1a3be9ef2e8b4d4737fdf6cdb1
DopeKS <kyusang.dev@gmail.com>
2024-04-22 15:01:52 +0900

    Initialize Project Memo

---
+++ Memo/Memo/MemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoApp.swift
+//  Memo
+//
+//  Created by 황규상 on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct MemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
