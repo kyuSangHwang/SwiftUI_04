b0d59790953285ca97e0504b4408712a39f33fa3
DopeKS <kyusang.dev@gmail.com>
2024-04-22 14:47:55 +0900

    Update SwiftData

--- SwiftDataDemo/SwiftDataDemo/ContentView.swift
+++ SwiftDataDemo/SwiftDataDemo/ContentView.swift
@@ -30,6 +30,11 @@ struct ContentView: View {
             List {
                 ForEach(tasks) { task in
                     HStack {
+                        Button(action: {
+                            toggleCompletion(for: task)
+                        }) {
+                            Image(systemName: task.completed ? "checkmark.circle.fill" : "circle")
+                        }
                         Text(task.title)
                         Spacer()
                         if task.completed {
@@ -63,6 +68,11 @@ struct ContentView: View {
     
     func deleteTask(_ task: Task) {
         modelContext.delete(task)
+    }
+    
+    func toggleCompletion(for task: Task) {
+        var updateTask = task
+        updateTask.completed.toggle()
     }
 }
 #Preview {
