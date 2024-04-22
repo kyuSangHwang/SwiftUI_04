cb05006d5044448d4c681a70249de51dc44f2c4e
DopeKS <kyusang.dev@gmail.com>
2024-04-22 14:10:04 +0900

    Task UI without SwiftData

--- SwiftDataDemo/SwiftDataDemo/ContentView.swift
+++ SwiftDataDemo/SwiftDataDemo/ContentView.swift
@@ -7,18 +7,56 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+class Task: Identifiable {
+    var id: UUID
+    var title: String
+    var completed: Bool
+
+    init(id: UUID = UUID(), title: String, completed: Bool = false) {
+        self.id = id
+        self.title = title
+        self.completed = completed
     }
 }
 
+struct ContentView: View {
+    @State var tasks: [Task] = [
+        Task(title: "Title1"),
+        Task(title: "Title2"),
+        Task(title: "Title3"),
+        Task(title: "Title4"),
+        Task(title: "Title5"),
+    ]
+
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(tasks) { task in
+                    HStack {
+                        Text(task.title)
+                        Spacer()
+                        if task.completed {
+                            Image(systemName: "checkmark")
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Tasks")
+            .toolbar {
+                ToolbarItem(placement:.topBarTrailing) {
+                    Button(action: addTask) {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
+        }
+    }
+
+    func addTask() {
+        let newTask = Task(title: "New Task")
+        self.tasks.append(newTask)
+    }
+}
 #Preview {
     ContentView()
 }
